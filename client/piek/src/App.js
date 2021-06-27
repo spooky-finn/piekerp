5ff85fd2063a7611e5ba6d4a800a967778777558
masha <57405750+Massshhhaaa@users.noreply.github.com>
2021-06-27 22:04:12 +0300

    init

---
+++ client/piek/src/App.js
@@ -0,0 +1,58 @@
+import React, { useContext, useEffect, useState} from 'react';
+import { Context } from './index';
+import LoginForm from './components/LoginForm';
+import {observer} from 'mobx-react-lite';
+import UserService from './services/UserService';
+
+function App() {
+  const {store} = useContext(Context);
+  const [users, setUsers] = useState();
+  // const usersList = ;
+  
+  
+
+  useEffect( () => {
+    if (localStorage.getItem('token')){
+      store.checkAuth();
+    }
+  }, [])
+
+  async function getUsers(){
+    try {
+      const response = await UserService.fetchUsers();
+      setUsers(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  if (store.isLoading){
+    return(
+      <div>Загрузка</div>
+    )
+  }
+  if (!store.isAuth) {
+    return (
+      <LoginForm/>
+    )
+  }
+
+  return (
+    <div className="App">
+          <h1>{store.isAuth ? `Пользователь авторизован ${store.user?.Email}` : 'Авторизуйтесь'}</h1>
+          <button onClick={() => store.logout()}>Выйти</button>
+
+          <div>
+            <button onClick={getUsers}>Get USers</button>
+          </div>
+          <div>
+          {users?.map((user, index) => (
+            <div key={index}>{user.Email} -  {user.FirstName} {user.LastName}</div>
+          ))}
+        </div>
+      
+    </div>
+  );
+}
+
+export default observer(App);
---
+++ client/piek/src/components/LoginForm.js
@@ -0,0 +1,30 @@
+import { observer } from 'mobx-react-lite';
+import React, { useState, useContext } from 'react';
+import { Context } from '../index';
+
+
+const LoginForm = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const {store} = useContext(Context);
+
+    return(
+        <div>
+            <input 
+            onChange={e => setEmail(e.target.value)}
+            value={email}
+            type='text'
+            placeholder='Email' />
+
+            <input 
+            onChange={e => setPassword(e.target.value)}
+            value={password}
+            type='text'
+            placeholder='Password' />
+
+            <button onClick={() => store.login(email, password)}>Log in</button>
+        </div>
+    );
+}
+
+export default observer(LoginForm);
