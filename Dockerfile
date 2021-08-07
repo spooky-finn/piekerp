f2eb49a761fdf86e1210871b66a5782dbaa1b566
masha <57405750+Massshhhaaa@users.noreply.github.com>
2021-08-07 18:22:02 +0300

    docker setup

---
+++ Dockerfile
@@ -0,0 +1,28 @@
+# Setup and build the client
+
+FROM node:16-alpine3.11 as client
+
+WORKDIR /app/client/
+COPY client/piek/package*.json ./
+RUN npm install 
+COPY client/piek ./
+RUN npm run build
+
+
+# Setup the server
+
+FROM node:16-alpine3.11
+
+WORKDIR /app/
+COPY --from=client /app/client/build/ ./client/build/
+
+WORKDIR /app/server/
+COPY server/package*.json ./
+RUN npm install 
+COPY server/ ./
+
+ENV PORT 9000
+
+EXPOSE 9000
+
+CMD ["npm", "start"]
--- server/app.js
+++ server/app.js
@@ -8,11 +8,15 @@ var cors = require("cors");
 
 const errorMiddleware = require('./middlewares/error-middleware');
 
+const CLIENT_BUILD_PATH = '/app/client/build';
 
 var indexRouter = require('./routes/index');
 
 
 var app = express();
+
+// Static files
+app.use(express.static(CLIENT_BUILD_PATH));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -37,23 +41,26 @@ app.use(errorMiddleware);
 
 
 
-
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// All remaining requests return the React app, so it can handle routing.
+app.get('*', function(request, response) {
+  response.sendFile(path.join(CLIENT_BUILD_PATH, 'index.html'));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 
 
