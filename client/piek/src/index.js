2f032837d0aa6c2568d2323b48dafa325a4032ff
masha <57405750+Massshhhaaa@users.noreply.github.com>
2021-09-02 17:06:38 +0300

    apollo JWT validation middleware.

--- client/piek/src/index.js
+++ client/piek/src/index.js
@@ -1,17 +1,27 @@
-import {WebSocketLink} from 'apollo-link-ws';
 import React, { createContext } from 'react';
 import ReactDOM from 'react-dom';
-import './theme.css';
-import './index.sass';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Store from './store/store';
-import 'react-notifications/lib/notifications.css';
+// import 'react-notifications/lib/notifications.css';
 import { BrowserRouter } from 'react-router-dom';
+import jwt from 'jsonwebtoken'
+//UI
+import './theme.css';
+import './index.sass';
+
+//apollo 
+import { WebSocketLink} from 'apollo-link-ws';
+import { onError } from "@apollo/client/link/error";
+import { ApolloLink, Observable } from 'apollo-link';  // add Observable
+import { setContext } from "@apollo/client/link/context";
+
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
+  from,
 } from "@apollo/client";
 
 
@@ -22,17 +32,14 @@ export const Context = createContext({
 });
 
 
-const webSocketLink = new WebSocketLink({
+let webSocketLink = new WebSocketLink({
   uri: process.env.REACT_APP_HASURA_WS,
   options: {
     reconnect: true,
     lazy: true,
-    minTimeout: 1000,
-    inactivityTimeout: 30000,
-    connectionCallback: async () => {
-        console.log('gql error. Request new access token');
-        await store.checkAuth()
-    },
+    reconnectionAttempts: 5,
+    inactivityTimeout: 1000,
+    connectionCallback: async (error) => { error && console.log(error) },
     connectionParams: () => ({
       headers: {
         'Authorization' : `Bearer ${store.inMemoryToken}`
@@ -42,11 +49,30 @@ const webSocketLink = new WebSocketLink({
   }
 })
 
+const asyncTokenValidation = setContext( async (_, {headers, ...context}) => {
+  var token = null 
+
+  if (Date.now() >= jwt.decode(store.inMemoryToken).exp * 1000 ) {
+    console.log('Existed access token was expired, request for new token..')
+    token = await store.getNewToken()
+  }
+
+  return {
+    headers: {
+      ...headers,
+      ...(token ? {Authorization: `Bearer ${token}`} : {}),
+    },
+    ...context,
+  };
+});
+
 
 const apolloClient = new ApolloClient({
-  link: webSocketLink,
+  link: from([ asyncTokenValidation, webSocketLink ]),
   cache: new InMemoryCache()
 })
+
+
 
 const root = document.getElementById('root');
 
