d13b61e9b393303f0358a75c9a0346d138ecaafd
masha <57405750+Massshhhaaa@users.noreply.github.com>
2021-09-26 17:29:22 +0300

    settings popover

--- client/piek/src/components/Sidebar/Sidebar.js
+++ client/piek/src/components/Sidebar/Sidebar.js
@@ -1,15 +1,27 @@
 // import { observer } from 'mobx-react-lite';
 import { useContext } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useHistory } from 'react-router-dom';
 import { Context } from '../../index';
 
 import { Pane } from 'evergreen-ui';
 import './index.sass';
-import { UilSortAmountDown, UilWrench, UilUserCircle, UilSignInAlt, UilConstructor} from '@iconscout/react-unicons';
-import { DarkToggle } from './DarkToggle';
+import sass from './index.module.sass'
+
+import { UilSortAmountDown, UilWrench, UilUserCircle, UilSignInAlt, UilConstructor, UilSetting} from '@iconscout/react-unicons';
+
+import { Popover, Position, Menu, CircleArrowRightIcon, EditIcon, Button } from 'evergreen-ui'
+
+import { SystemPreferTheme } from '../core/SystemPreferTheme';
 
 const Sidebar = () => {
     const {store} = useContext(Context);
+    const [isDark, setIsDark] = SystemPreferTheme();
+    const history = useHistory()
+
+    async function logout(){
+        await store.logout(); 
+        history.push('/login')
+    }
 
     return(
         <>
@@ -18,9 +30,26 @@ const Sidebar = () => {
             <NavLink  exact  to='/' activeClassName='sidebar-active' className="action-icon"><UilSortAmountDown/></NavLink>
             <NavLink  to='/reclamation' activeClassName='sidebar-active' className="action-icon"><UilWrench/></NavLink>
             <NavLink  to='/attendance' activeClassName='sidebar-active' className="action-icon"><UilConstructor/></NavLink>
-            <a className="action-icon hidden" ><DarkToggle/></a> 
-            <NavLink  className="action-icon hidden" to='/account'><UilUserCircle/></NavLink>
-            <NavLink  className="action-icon hidden" to='/login' onClick={() => store.logout()}><UilSignInAlt/></NavLink>
+                
+            <Popover
+            content={
+                <Menu>
+                <Menu.Group>
+                    <Menu.Item onClick={() => setIsDark(!isDark)}>Переключить тему</Menu.Item>
+                </Menu.Group>
+                <Menu.Divider />
+
+                <Menu.Group>
+                    <Menu.Item disabled>{store.user.FirstName}  {store.user.LastName}</Menu.Item>
+                    <Menu.Item className={sass.logoutButton} onClick={logout} >Выйти</Menu.Item>
+                </Menu.Group>
+
+                </Menu>
+                }
+                >
+                <div appearance="minimal" className="action-icon settings"><UilSetting/></div>
+            </Popover>   
+
         </Pane>
         </>
     );
