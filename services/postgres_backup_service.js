3bd35387043761637720cea3012f29f31d9084a8
Massshhhaaa <loseev5@gmail.com>
2021-10-05 23:22:02 +0300

    services

--- services/postgres_backup_service.js
+++ services/postgres_backup_service.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 require('dotenv').config(); 
 const fs = require('fs');
 const { exec } = require('child_process');
@@ -11,15 +12,14 @@ const s3 = new AWS.S3({
   signatureVersion: 'v4',
 });
 
-const fileName = '.env';
 
-const uploadFile = () => {
+const uploadFile = (fileName) => {
 
   fs.readFile(fileName, (err, data) => {
      if (err) throw err;
      const params = {
          Bucket: 'piek-factory-backup', // pass your bucket name
-         Key: 'postgres-backup',
+         Key: 'pg-backup ' + new Date(),
          Body: JSON.stringify(data, null, 2)
      };
      s3.upload(params, function(s3Err, data) {
@@ -30,14 +30,15 @@ const uploadFile = () => {
 };
 
 function intervalFunc() {
-  const exexCont = "sudo docker exec hasura_postgres_1 ls"
-  exec(exexCont, (err, stdout) => {
-    if (err) console.log(err)
+  const execCont = "docker exec hasura_postgres_1 pg_dump -U postgres -d postgres > dump.sql"
+  exec(execCont, (err, stdout, stderr) => {
+    if (err || stderr) console.log(err, stderr)
     else{
-      uploadFile()
+     console.log('pg dump created.');
+      uploadFile('dump.sql')
       console.log(stdout)
     }
   })
 }
 
-setInterval(intervalFunc, 3000);
+setInterval(intervalFunc, 1000*5)
