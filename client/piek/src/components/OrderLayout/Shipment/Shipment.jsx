50f7a0f839b16c77a7e0c626f1c9580d4bbd220f
masha <57405750+Massshhhaaa@users.noreply.github.com>
2021-10-11 15:00:39 +0300

    payment history, UI update, build, bags fixing

--- client/piek/src/components/OrderLayout/Shipment/Shipment.jsx
+++ client/piek/src/components/OrderLayout/Shipment/Shipment.jsx
@@ -7,6 +7,7 @@ import { useHistory } from "react-router-dom";
 import { 
   UPDATE_AWAITING_DISPATCH,
   UPDATE_ORDER_STATUS } from '../queries/MutationOrderInfo'
+import { DELETE_ORDER } from '../queries/MutationDeleteOrder';
 
 const Shipment = (props) => {
   const { data, orderID } = props
@@ -14,9 +15,17 @@ const Shipment = (props) => {
   const history = useHistory();
   const [mutateAwaitingDispatch] = useMutation(UPDATE_AWAITING_DISPATCH);
   const [mutateOrderStatus] = useMutation(UPDATE_ORDER_STATUS);
+  const [deleteOrder] = useMutation(DELETE_ORDER);
 
-  
-
+  function deleteOrderHandler(){
+    deleteOrder({ variables: {
+      OrderID: orderID
+    }}).then( res => {
+      if (res.data.delete_erp_Orders_by_pk.OrderID){
+        history.push("/")
+      }
+    })
+  }
   function awaitingDispatchHandler(){
     mutateAwaitingDispatch({
       variables: {orderID, awaitingDispatch: !data.AwaitingDispatch },
@@ -30,7 +39,10 @@ const Shipment = (props) => {
     })
   }
   async function TransferOrder(){
-    mutateOrderStatus({ variables: { OrderID: orderID }}).then(
+    mutateOrderStatus({ variables: { 
+      OrderID: orderID,
+      ActualShippingDate: new Date()
+     }}).then(
       (res) =>{
         if (res.data.update_erp_Orders_by_pk.OrderStatusID === 3){
           history.push("/")
@@ -39,7 +51,12 @@ const Shipment = (props) => {
     )
   }
 
-  if (data.OrderStatusID !== 2) return null
+  if (data.OrderStatusID === 3) return null
+  if (data.OrderStatusID === 1) return (
+    <div>
+        <Button onClick={deleteOrderHandler} className={sass.btn} appearance="minimal">Удалить предзаказ</Button>
+    </div>
+  )
   return (
     <div>
         <CornerDialog
@@ -52,7 +69,7 @@ const Shipment = (props) => {
         >
           Заказ удалится из очередности, но его в любое время можно будет найти в архиве по номеру счета, юрлицу, маркировке привода.
         </CornerDialog>
-        <Checkbox className={sass.checkbox} label="Ожидает отгрузки" onChange={awaitingDispatchHandler} checked={data.AwaitingDispatch}/>
+        <Checkbox className={sass.checkbox} marginTop={0} label="Ожидает отгрузки" onChange={awaitingDispatchHandler} checked={data.AwaitingDispatch}/>
         <Button onClick={() => setIsShown(true)} className={sass.btn} appearance="primary">Отгружен</Button>
     </div>
   )
